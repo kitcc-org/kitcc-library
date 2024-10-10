1cf15638f9c214f747cbf404c48aa6ca9bb4d544
shunsei <kimshun1227@icloud.com>
2024-10-11 03:59:21 +0900

    Write draft auth api  (#11)

---
+++ backend/src/api/auth.ts
@@ -0,0 +1,65 @@
+import { userTable } from '@/drizzle/schema';
+import { zValidator } from '@hono/zod-validator';
+import { eq } from 'drizzle-orm';
+import { drizzle } from 'drizzle-orm/d1';
+import { Hono } from 'hono';
+import { loginBody, loginResponse } from '../schema';
+import { isLoggedIn, login, logout } from '../utils/auth';
+import { generateHash } from '../utils/crypto';
+
+const app = new Hono<{ Bindings: Env }>();
+
+app.post(
+	'/',
+	zValidator('json', loginBody, (result, ctx) => {
+		if (!result.success) {
+			return ctx.json(
+				{
+					message: 'Request Body Validation Error',
+				},
+				400
+			);
+		}
+	}),
+	async (ctx) => {
+		const credential = ctx.req.valid('json');
+
+		const db = drizzle(ctx.env.DB);
+		const user = await db
+			.select()
+			.from(userTable)
+			.where(eq(userTable.email, credential.email));
+
+		const loggedIn = await isLoggedIn(ctx);
+		if (!loggedIn) {
+			const hash = generateHash(credential.password);
+			if (hash === user[0].passwordDigest) {
+				login(ctx, user[0].id);
+			} else {
+				return ctx.json(
+					{
+						message: 'Unauthorized',
+					},
+					401
+				);
+			}
+		}
+
+		const result = loginResponse.safeParse(user[0]);
+		if (!result.success) {
+			return ctx.json(
+				{
+					message: 'Response Validation Error',
+				},
+				500
+			);
+		} else {
+			return ctx.json(result.data);
+		}
+	}
+);
+
+app.delete('/', async (ctx) => {
+	logout(ctx);
+	return ctx.body(null, 204);
+});
---
+++ backend/src/utils/auth.ts
@@ -0,0 +1,58 @@
+import { userTable } from '@/drizzle/schema';
+import { eq } from 'drizzle-orm';
+import { drizzle } from 'drizzle-orm/d1';
+import { Context } from 'hono';
+import { deleteCookie, getCookie, setCookie } from 'hono/cookie';
+
+export const isLoggedIn = async (ctx: Context) => {
+	const userId = getCookie(ctx, 'user_id', 'secure');
+	if (userId === undefined) {
+		return false;
+	}
+
+	const sessionToken = getCookie(ctx, 'session_token', 'secure');
+	if (sessionToken === undefined) {
+		return false;
+	}
+
+	const db = drizzle(ctx.env.DB);
+	const user = await db
+		.select(userTable.sessionToken)
+		.from(userTable)
+		.where(eq(userTable.id, parseInt(userId)));
+
+	if (0 < user.length) {
+		return user[0].sessionToken === sessionToken;
+	} else {
+		return false;
+	}
+};
+
+export const login = async (ctx: Context, userId: string) => {
+	setCookie(ctx, 'user_id', userId, { secure: true, prefix: 'secure' });
+
+	const sessionToken = crypto.randomUUID();
+	const db = drizzle(ctx.env.DB);
+	await db
+		.update(userTable)
+		.set({ sessionToken: sessionToken })
+		.where(eq(userTable.id, userId));
+	// prettier-ignore
+	setCookie(ctx, 'session_token', sessionToken, { secure: true, prefix: 'secure' });
+};
+
+export const logout = async (ctx: Context) => {
+	const userId = getCookie(ctx, 'user_id', 'secure');
+	if (userId === undefined) {
+		return;
+	}
+
+	const db = drizzle(ctx.env.DB);
+	await db
+		.update(userTable)
+		.set({ sessionToken: null })
+		.where(eq(userTable.id, userId));
+
+	deleteCookie(ctx, 'user_id', { secure: true });
+	deleteCookie(ctx, 'session_token', { secure: true });
+};
