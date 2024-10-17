ebdb451f6082471d9deb0301f7bedf1b6ee55de4
shunsei <kimshun1227@icloud.com>
2024-10-17 13:33:41 +0900

    Add cors setting  (#28)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
 import { prettyJSON } from 'hono/pretty-json';
 import auth from './api/auth';
@@ -9,6 +10,27 @@ const app = new Hono();
 
 app.use(logger());
 app.use(prettyJSON());
+app.use(
+	'*',
+	cors({
+		origin: (origin: string) => {
+			return origin.includes('localhost')
+				? origin
+				: // TODO: フロントエンドのデプロイ先のURLを指定
+				  'https://frontend.pages.dev';
+		},
+		// リクエストに含めることができるヘッダ
+		allowHeaders: ['Cookie', 'Content-Type'],
+		// 許可するメソッド
+		// OPTIONSはCORSのプリフライトリクエストで使われる
+		allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+		// アクセスできるレスポンスのヘッダ
+		exposeHeaders: ['Content-Type', 'Content-Length', 'Set-Cookie'],
+		// 資格情報をフロントエンドのJSに公開するか
+		// 資格情報とはCookie, 認証ヘッダー, TLSクライアント証明書
+		credentials: true,
+	})
+);
 
 app.route('/books', book);
 app.route('/users', user);
