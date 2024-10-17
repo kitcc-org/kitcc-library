ea4e28450ce961cd9ead1f9fc500ba9238aa9119
shunsei <kimshun1227@icloud.com>
2024-10-17 19:30:10 +0900

    Add users & books relation  (#12)

--- backend/drizzle/migrations/0006_mysterious_boomerang.sql
+++ backend/drizzle/migrations/0006_mysterious_boomerang.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `loans` (
-	`book_id` integer NOT NULL,
 	`user_id` integer NOT NULL,
+	`book_id` integer NOT NULL,
 	`volume` integer DEFAULT 1 NOT NULL,
 	`created_at` text DEFAULT (datetime(current_timestamp, '+9 hours')) NOT NULL,
 	`updated_at` text DEFAULT (datetime(current_timestamp, '+9 hours')) NOT NULL,
 	PRIMARY KEY(`book_id`, `user_id`),
-	FOREIGN KEY (`book_id`) REFERENCES `books`(`id`) ON UPDATE no action ON DELETE cascade,
-	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`book_id`) REFERENCES `books`(`id`) ON UPDATE no action ON DELETE cascade
 );
