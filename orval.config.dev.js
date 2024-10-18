47b1b1eb1a15dd600696f6cbf48d8f733accf241
Kosei805 <na913420ka85@gmail.com>
2024-10-18 22:36:28 +0900

    remove; orval library config

--- orval.config.dev.js
+++ orval.config.dev.js
@@ -1,14 +1,4 @@
 module.exports = {
-  library: {
-    input: {
-      target: './api/bundle.yml',
-    },
-    output: {
-      client: 'zod',
-      mode: 'single',
-      target: './backend/src/schema.ts',
-    },
-  },
   frontend: {
     input: {
       target: './api/bundle.yml',
