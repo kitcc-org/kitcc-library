97baae70564282a08169c701c6ec640c64b8bd12
Kosei805 <na913420ka85@gmail.com>
2024-10-19 18:16:09 +0900

    fix: file name

---
+++ frontend/app/components/login/LoginFormTitle.tsx
@@ -0,0 +1,15 @@
+import { Text } from '@mantine/core'
+
+const LoginFormTitle = () => {
+  return (
+    <Text
+      ta='center'
+      tt='uppercase'
+      size='xl'
+    >
+      ログイン
+    </Text>
+  )
+}
+
+export default LoginFormTitle
