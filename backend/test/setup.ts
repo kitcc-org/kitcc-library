5abe2cf1748a5c1f6413970e062b4ffc5b4af3ed
shunsei <kimshun1227@icloud.com>
2024-10-22 19:25:51 +0900

    Update test setup script  (#60)

--- backend/test/setup.ts
+++ backend/test/setup.ts
@@ -5,15 +5,17 @@ const db = env.DB;
 await db
 	.prepare(
 		`
-    CREATE TABLE IF NOT EXISTS books (
-      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-      title TEXT NOT NULL,
-      authors TEXT NOT NULL,
-      publisher TEXT NOT NULL,
-      thumbnail TEXT,
-      isbn TEXT NOT NULL,
-      stock INTEGER NOT NULL DEFAULT 1
-    )
+    CREATE TABLE books (
+			id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+			title TEXT NOT NULL,
+			authors TEXT NOT NULL,
+			publisher TEXT NOT NULL,
+			published_date TEXT NOT NULL,
+			description TEXT,
+			thumbnail TEXT,
+			isbn TEXT NOT NULL,
+			stock INTEGER DEFAULT 1 NOT NULL
+		);
     `,
 	)
 	.run();
