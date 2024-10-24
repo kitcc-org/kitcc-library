ceae16167bf634d18ac64c2799baa390571f5dd5
shunsei <kimshun1227@icloud.com>
2024-10-24 15:10:18 +0900

    Change dialog add cart button color  (#78)

--- frontend/app/components/books/BookCardCartButton.tsx
+++ frontend/app/components/books/BookCardCartButton.tsx
@@ -1,26 +1,23 @@
-import { Button } from '@mantine/core'
-import { useAtom } from 'jotai';
+import { Button } from "@mantine/core";
+import { useAtom } from "jotai";
 import { BiSolidCartAdd } from "react-icons/bi";
-import { cartAtom } from '~/stores/cartAtom';
+import { cartAtom } from "~/stores/cartAtom";
 
 interface BookCardCartButtonProps {
-  id: number
-  stock: number
+  id: number;
+  stock: number;
 }
 
-const BookCardCartButton = ({
-  id,
-  stock
-}: BookCardCartButtonProps) => {
-  const [cart, setCart] = useAtom(cartAtom)
+const BookCardCartButton = ({ id, stock }: BookCardCartButtonProps) => {
+  const [cart, setCart] = useAtom(cartAtom);
   const addCart = () => {
-    setCart([...cart, { id, stock }])
-  }
+    setCart([...cart, { id, stock }]);
+  };
   return (
     <Button
-      variant='filled'
-      color='yellow'
-      radius='xl'
+      variant="filled"
+      color="yellow"
+      radius="xl"
       disabled={stock === 0}
       onClick={addCart}
       leftSection={<BiSolidCartAdd />}
@@ -28,7 +25,7 @@ const BookCardCartButton = ({
     >
       カートに入れる
     </Button>
-  )
-}
+  );
+};
 
-export default BookCardCartButton
+export default BookCardCartButton;
--- frontend/app/components/books/BookSelectedDialog.tsx
+++ frontend/app/components/books/BookSelectedDialog.tsx
@@ -1,15 +1,14 @@
-import { Button, Center, Dialog, Group, Stack, Text } from '@mantine/core'
-import React from 'react'
-import { selectedBooksAtom } from '~/stores/cartAtom'
-import { useAtom } from 'jotai'
+import { Button, Center, Dialog, Stack, Text } from "@mantine/core";
+import { useAtom } from "jotai";
+import { selectedBooksAtom } from "~/stores/cartAtom";
 
 const BookSelectedDialog = () => {
-  const [selectedBook, setSelectedBook] = useAtom(selectedBooksAtom)
+  const [selectedBook, setSelectedBook] = useAtom(selectedBooksAtom);
   return (
     <Dialog
       opened={selectedBook.length > 0}
       onClose={() => setSelectedBook([])}
-    // size='650px'
+      // size='650px'
     >
       <Stack
         bg="var(--mantine-color-body)"
@@ -18,22 +17,25 @@ const BookSelectedDialog = () => {
         gap="md"
       >
         <Center>
-          <Text fw={500}>
-            選択中の本が{selectedBook.length}冊あります
-          </Text>
+          <Text fw={500}>選択中の本が{selectedBook.length}冊あります</Text>
         </Center>
-        <Button fz='xs'>
+        <Button fz="xs" color="yellow">
           選択中の本をカートに入れる
         </Button>
-        <Button fz='xs' color='red'>
+        <Button fz="xs" color="red">
           選択中の本を削除する
         </Button>
-        <Button fz='xs' variant='light' onClick={() => setSelectedBook([])}>
+        <Button
+          fz="xs"
+          variant="light"
+          style={{ border: "solid" }}
+          onClick={() => setSelectedBook([])}
+        >
           選択を解除する
         </Button>
       </Stack>
     </Dialog>
-  )
-}
+  );
+};
 
-export default BookSelectedDialog
+export default BookSelectedDialog;
