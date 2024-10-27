d46b1ea508d9aacf60f71d3b78cad431df212f3d
shunsei <k.shunsei1227@gmail.com>
2024-10-27 13:47:30 +0900

    Fix client import  (#87)

--- frontend/app/components/book-detail/BookDetailContent.tsx
+++ frontend/app/components/book-detail/BookDetailContent.tsx
@@ -1,31 +1,31 @@
-import { Stack } from '@mantine/core'
-import { Book } from 'orval/client.schemas'
-import BookDetailTitle from './BookDetailTitle'
-import BookDetailContentTable from './BookDetailContentTable'
-import BookDetailDescription from './BookDetailDescription'
-import { useAtom } from 'jotai'
-import { userAtom } from '~/stores/userAtom'
-import BookDetailBorrower from './BookDetailBorrower'
+import { Stack } from "@mantine/core";
+import { Book } from "client/client.schemas";
+import { useAtom } from "jotai";
+import { userAtom } from "~/stores/userAtom";
+import BookDetailBorrower from "./BookDetailBorrower";
+import BookDetailContentTable from "./BookDetailContentTable";
+import BookDetailDescription from "./BookDetailDescription";
+import BookDetailTitle from "./BookDetailTitle";
 
 interface BookDetailComponentProps {
-  book: Book
+  book: Book;
 }
 
 const BookDetailContent = ({ book }: BookDetailComponentProps) => {
-  const [user] = useAtom(userAtom)
+  const [user] = useAtom(userAtom);
   return (
     <Stack
       bg="var(--mantine-color-body)"
       align="stretch"
-      justify='flex-start'
-      gap='xl'
+      justify="flex-start"
+      gap="xl"
     >
       <BookDetailTitle title={book.title} />
       <BookDetailContentTable book={book} />
       <BookDetailDescription description={book.description} />
       {!!user && <BookDetailBorrower />}
     </Stack>
-  )
-}
+  );
+};
 
-export default BookDetailContent
+export default BookDetailContent;
