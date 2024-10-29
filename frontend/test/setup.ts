090e30dfbcdac9e61c5d9c9a278f5f48211943d4
shunsei <k.shunsei1227@gmail.com>
2024-10-29 16:36:34 +0900

    Remove frontend test  (#100)

--- frontend/test/setup.ts
+++ frontend/test/setup.ts
@@ -3,19 +3,6 @@ import { cleanup } from '@testing-library/react';
 import { setupServer } from 'msw/node';
 import { handlers } from './mocks/handlers';
 
-vi.mock('@remix-run/react', () => {
-	const useNavigate = vi.fn();
-	const form = vi
-		.fn()
-		.mockImplementation(({ children }: { children: React.ReactElement }) => {
-			return children;
-		});
-	return {
-		useNavigate: useNavigate,
-		Form: form,
-	};
-});
-
 const server = setupServer(...handlers);
 
 beforeAll(() => {
