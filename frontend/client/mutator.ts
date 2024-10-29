dcf3470031439aff16eaec91038fd3cc79d1475f
Kosei805 <na913420ka85@gmail.com>
2024-10-29 17:12:58 +0900

    remove credentials

--- frontend/client/mutator.ts
+++ frontend/client/mutator.ts
@@ -1,20 +1,17 @@
 export const customFetch = async <T>(
-  url: string,
-  options?: RequestInit
+	url: string,
+	options?: RequestInit,
 ): Promise<T> => {
-  const res = await fetch(url, {
-    ...options,
-    // 異なるオリジンからのCookieを保存する
-    // 異なるオリジンへCookieを送信する
-    credentials: "include",
-  });
+	const res = await fetch(url, {
+		...options,
+	});
 
-  const response: { status: number; data?: unknown } = { status: res.status };
-  const contentType = res.headers.get("content-type");
+	const response: { status: number; data?: unknown } = { status: res.status };
+	const contentType = res.headers.get('content-type');
 
-  if (contentType?.includes("application/json")) {
-    response.data = await res.json();
-  }
+	if (contentType?.includes('application/json')) {
+		response.data = await res.json();
+	}
 
-  return response;
+	return response;
 };
