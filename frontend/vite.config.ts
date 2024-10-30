bd2e324699028397cee1885f3d999df309e1599b
shunsei <k.shunsei1227@gmail.com>
2024-10-30 17:30:50 +0900

    Install @remix-run/testing  (#102)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,41 +1,41 @@
 /// <reference types="vitest" />
 import {
-  vitePlugin as remix,
-  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
-} from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+	vitePlugin as remix,
+	cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+} from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [
-    remixCloudflareDevProxy(),
-    !process.env.VITEST
-      ? remix({
-          future: {
-            v3_fetcherPersist: true,
-            v3_relativeSplatPath: true,
-            v3_throwAbortReason: true,
-          },
-        })
-      : null,
-    tsconfigPaths(),
-  ],
-  server: {
-    proxy: {},
-    https: {
-      key: "./certs/key.pem",
-      cert: "./certs/cert.pem",
-    },
-  },
-  test: {
-    alias: {
-      "~/*": "./app/*",
-    },
-    env: {
-      NODE_TLS_REJECT_UNAUTHORIZED: "0",
-    },
-    environment: "happy-dom",
-    globals: true,
-    setupFiles: ["./test/setup.ts"],
-  },
+	plugins: [
+		remixCloudflareDevProxy(),
+		!process.env.VITEST
+			? remix({
+					future: {
+						v3_fetcherPersist: true,
+						v3_relativeSplatPath: true,
+						v3_throwAbortReason: true,
+					},
+				})
+			: null,
+		tsconfigPaths(),
+	],
+	server: {
+		proxy: {},
+		https: {
+			key: './certs/key.pem',
+			cert: './certs/cert.pem',
+		},
+	},
+	test: {
+		alias: {
+			'~/*': './app/*',
+		},
+		env: {
+			NODE_TLS_REJECT_UNAUTHORIZED: '0',
+		},
+		environment: 'happy-dom',
+		globals: true,
+		setupFiles: ['./test/setup.ts'],
+	},
 });
