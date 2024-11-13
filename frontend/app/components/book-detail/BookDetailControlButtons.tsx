2c823be993b9e9fe2bf10954bcd4495c39c8abf4
Kosei805 <na913420ka85@gmail.com>
2024-11-13 22:36:05 +0900

    front; book delete button

--- frontend/app/components/book-detail/BookDetailControlButtons.tsx
+++ frontend/app/components/book-detail/BookDetailControlButtons.tsx
@@ -2,30 +2,17 @@ import BookDetailEditButton from './BookDetailEditButton';
 import { MdDeleteForever } from 'react-icons/md';
 import { Button } from '@mantine/core';
 import { useFetcher } from '@remix-run/react';
+import BookDetailDeleteButton from './BookDetailDeleteButton';
 
 interface BookDetailControlButtonsProps {
 	id: number;
 }
 
 const BookDetailControlButtons = ({ id }: BookDetailControlButtonsProps) => {
-	const fetcher = useFetcher();
 	return (
 		<>
 			<BookDetailEditButton bookId={id} />
-			<Button
-				color="red"
-				leftSection={<MdDeleteForever />}
-				fz="lg"
-				onClick={() =>
-					fetcher.submit(
-						{ bookId: id },
-						{ action: '/home/books/$bookId', method: 'DELETE' },
-					)
-				}
-				disabled={fetcher.state === 'submitting'}
-			>
-				削除
-			</Button>
+			<BookDetailDeleteButton bookId={id} />
 		</>
 	);
 };
