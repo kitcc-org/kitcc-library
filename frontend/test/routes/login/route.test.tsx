70e80e7a37e70bffd031b008c5b1fc8863d9da2d
shunsei <k.shunsei1227@gmail.com>
2024-11-15 13:28:53 +0900

    Make login test directory  (#89)

--- frontend/test/routes/login/route.test.tsx
+++ frontend/test/routes/login/route.test.tsx
@@ -3,8 +3,8 @@ import { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/cloudflare';
 import { createRemixStub } from '@remix-run/testing';
 import { screen } from '@testing-library/react';
 import LoginPage, { action, loader } from '~/routes/login/route';
-import { customRender } from '../helpers/wrapper';
-import { redirect } from '../mocks/@remix-run/cloudflare';
+import { customRender } from '../../helpers/wrapper';
+import { redirect } from '../../mocks/@remix-run/cloudflare';
 
 vi.mock('@remix-run/cloudflare', async (importOriginal) => {
 	const actual = await importOriginal<typeof remixruncloudflare>();
