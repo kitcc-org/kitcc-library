b359e8ba451befc8a48ece83806883197e5eae91
Kosei805 <na913420ka85@gmail.com>
2024-11-17 23:00:13 +0900

    fix: cart handlers

--- frontend/app/components/cart/CartCardHeader.tsx
+++ frontend/app/components/cart/CartCardHeader.tsx
@@ -10,16 +10,14 @@ interface CartCardHeaderProps {
 	thumbnail?: string;
 }
 
-const CartCardHeader = ({
-	id,
-	stock,
-	volume,
-	thumbnail,
-}: CartCardHeaderProps) => {
+const CartCardHeader = ({ id, stock, volume }: CartCardHeaderProps) => {
 	const [cart, setCart] = useAtom(cartAtom);
 	const [selectedCartBook, setSelectedCartBook] = useAtom(
 		selectedCartBooksAtom,
 	);
+
+	//  選択されている本のIDと表示する本のIDを比較する関数
+	const isSelected = (element: CartProps) => element.id === id;
 
 	// 該当する本のvolumeを変更する
 	const handleChangeVolume = (id: number, value: number) => {
@@ -36,24 +34,38 @@ const CartCardHeader = ({
 				return element;
 			}),
 		);
+		const target = selectedCartBook.find(isSelected);
+		// volumeを選択されていた本がすでに選択されている場合
+		if (target) {
+			setSelectedCartBook(
+				selectedCartBook.map((element) => {
+					if (element.id === id) {
+						return {
+							id: element.id,
+							stock: element.stock,
+							thumbnail: element.thumbnail,
+							volume: value,
+						};
+					}
+					return element;
+				}),
+			);
+		}
 	};
 
-	//  選択されている本のIDと表示する本のIDを比較する関数
-	const isSelected = (element: CartProps) => element.id === id;
-
 	const switchBookSelect = () => {
-		// チェックボックスの状態が変化した時に
-		if (selectedCartBook.some(isSelected)) {
-			// すでに選択されていた場合は選択を外す
-			setSelectedCartBook(
-				selectedCartBook.filter((element) => element.id !== id),
-			);
-		} else {
-			// 選択されていなかった場合は選択する
-			setSelectedCartBook([
-				...selectedCartBook,
-				{ id, stock, thumbnail, volume: 1 },
-			]);
+		const target = cart.find(isSelected);
+		if (target) {
+			// チェックボックスの状態が変化した時に
+			if (selectedCartBook.some(isSelected)) {
+				// すでに選択されていた場合は選択を外す
+				setSelectedCartBook(
+					selectedCartBook.filter((element) => element.id !== id),
+				);
+			} else {
+				// 選択されていなかった場合は選択する
+				setSelectedCartBook([...selectedCartBook, target]);
+			}
 		}
 	};
 
