f306d0aa034e5415c4d74191c4a8ea40120f9797
Kosei805 <na913420ka85@gmail.com>
2024-11-19 14:07:09 +0900

    front: add user create action

--- frontend/app/routes/home.users.add/route.tsx
+++ frontend/app/routes/home.users.add/route.tsx
@@ -1,11 +1,17 @@
 import { useForm } from '@mantine/form';
 import { useClipboard } from '@mantine/hooks';
-import { LoaderFunctionArgs } from '@remix-run/cloudflare';
+import {
+	ActionFunctionArgs,
+	json,
+	LoaderFunctionArgs,
+} from '@remix-run/cloudflare';
 import { redirect, useSubmit } from '@remix-run/react';
+import { createUser } from 'client/client';
 import type { CreateUserBody } from 'client/client.schemas';
 import { useEffect, useState } from 'react';
 import UsersAddComponent from '~/components/users-add/UsersAddComponent';
 import { commitSession, getSession } from '~/services/session.server';
+import { ActionResponse } from '~/types/response';
 import { errorNotification, successNotification } from '~/utils/notification';
 import { passwordGen } from '~/utils/password';
 
@@ -24,6 +30,68 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 	}
 
 	return null;
+};
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+	const session = await getSession(request.headers.get('Cookie'));
+	const userData = session.get('user');
+
+	if (!userData) {
+		session.flash('error', 'ログインしてください');
+		return redirect('/login', {
+			headers: {
+				'Set-Cookie': await commitSession(session),
+			},
+		});
+	}
+
+	const cookieHeader = [
+		`__Secure-user_id=${userData.id};`,
+		`__Secure-session_token=${userData.sessionToken}`,
+	].join('; ');
+
+	const requestBody = await request.json<{ createUserBody: CreateUserBody }>();
+	const createUserBody = requestBody.createUserBody;
+	const response = await createUser(createUserBody, {
+		headers: { Cookie: cookieHeader },
+	});
+
+	switch (response.status) {
+		case 201:
+			session.flash('success', 'ユーザーを追加しました');
+			return redirect('/home/users', {
+				headers: {
+					'Set-Cookie': await commitSession(session),
+				},
+			});
+		case 401:
+			session.flash('error', 'ログインしてください');
+			return redirect('/login', {
+				headers: {
+					'Set-Cookie': await commitSession(session),
+				},
+			});
+		case 409:
+			session.flash('error', '同じメールアドレスのユーザーが既に存在します');
+			return json<ActionResponse>(
+				{ method: 'DELETE', status: response.status },
+				{
+					headers: {
+						'Set-Cookie': await commitSession(session),
+					},
+				},
+			);
+		default:
+			session.flash('error', 'ユーザーの追加に失敗しました');
+			return json<ActionResponse>(
+				{ method: 'POST', status: response.status },
+				{
+					headers: {
+						'Set-Cookie': await commitSession(session),
+					},
+				},
+			);
+	}
 };
 
 const UserAddPage = () => {
@@ -81,7 +149,11 @@ const UserAddPage = () => {
 		// Clipboradにパスワードが保存されてから30秒以内かどうか
 		if (clipborad.copied) {
 			// パスワードを生成してから30秒以内の場合
-			submit(props, { method: 'POST' });
+			submit(JSON.stringify({ createUserBody: props }), {
+				action: 'home/users/add',
+				method: 'POST',
+				encType: 'application/json',
+			});
 		} else {
 			// パスワードを生成していない or 生成してから30秒経過している場合
 			errorNotification('パスワードを生成してください');
