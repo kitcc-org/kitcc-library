c1064f7e28c47a90df4c3254b5ce3da5654693c5
shunsei <k.shunsei1227@gmail.com>
2024-11-22 23:40:00 +0900

    Add confirmation before bulk delete book  (#147)

--- frontend/app/components/books/BookSelectedDialog.tsx
+++ frontend/app/components/books/BookSelectedDialog.tsx
@@ -1,6 +1,9 @@
-import { Button, Center, Dialog, Stack, Text } from '@mantine/core';
+import { Button, Center, Dialog, Modal, Stack, Text, rem } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { useNavigate, useSubmit } from '@remix-run/react';
 import { useAtom } from 'jotai';
+import { MdDeleteForever } from 'react-icons/md';
+import { RiArrowGoBackLine } from 'react-icons/ri';
 import { selectedBooksAtom } from '~/stores/bookAtom';
 import { cartAtom } from '~/stores/cartAtom';
 import { addBooksToCart } from '~/utils/cart';
@@ -9,61 +12,90 @@ import { successNotification } from '~/utils/notification';
 const BookSelectedDialog = () => {
 	const [selectedBook, setSelectedBook] = useAtom(selectedBooksAtom);
 	const [cart, setCart] = useAtom(cartAtom);
-	const navigate = useNavigate();
 
+	const navigate = useNavigate();
 	const submit = useSubmit();
+	const [opened, { open, close }] = useDisclosure();
 
 	return (
-		<Dialog
-			opened={selectedBook.length > 0}
-			onClose={() => setSelectedBook([])}
-		>
-			<Stack
-				bg="var(--mantine-color-body)"
-				align="stretch"
-				justify="center"
-				gap="md"
+		<>
+			<Modal
+				opened={opened}
+				onClose={close}
+				title="本当に削除しますか？"
+				centered
 			>
 				<Center>
-					<Text fw={500}>選択中の本が{selectedBook.length}冊あります</Text>
-				</Center>
-				<Button
-					fz="xs"
-					color="yellow"
-					onClick={() => {
-						setCart(addBooksToCart(cart, selectedBook));
-						setSelectedBook([]);
-						successNotification('カートに追加しました');
-						navigate('/home/cart');
-					}}
-				>
-					選択中の本をカートに入れる
-				</Button>
-				<Button
-					fz="xs"
-					color="red"
-					onClick={() => {
-						submit(JSON.stringify({ selectedBook: selectedBook }), {
-							action: '/home?index',
-							method: 'DELETE',
-							encType: 'application/json',
-						});
+					<Button
+						leftSection={<RiArrowGoBackLine />}
+						color="gray"
+						mr={rem(10)}
+						onClick={close}
+					>
+						キャンセル
+					</Button>
+					<Button
+						leftSection={<MdDeleteForever size={23} />}
+						color="red"
+						onClick={() => {
+							submit(JSON.stringify({ selectedBook: selectedBook }), {
+								action: '/home?index',
+								method: 'DELETE',
+								encType: 'application/json',
+							});
 
-						setSelectedBook([]);
-					}}
+							setSelectedBook([]);
+						}}
+					>
+						削除する
+					</Button>
+				</Center>
+			</Modal>
+			<Dialog
+				opened={selectedBook.length > 0}
+				onClose={() => setSelectedBook([])}
+			>
+				<Stack
+					bg="var(--mantine-color-body)"
+					align="stretch"
+					justify="center"
+					gap="md"
 				>
-					選択中の本を削除する
-				</Button>
-				<Button
-					fz="xs"
-					variant="light"
-					bd="solid 2px"
-					onClick={() => setSelectedBook([])}
-				>
-					選択を解除する
-				</Button>
-			</Stack>
-		</Dialog>
+					<Center>
+						<Text fw={500}>選択中の本が{selectedBook.length}冊あります</Text>
+					</Center>
+					<Button
+						fz="sm"
+						color="yellow"
+						onClick={() => {
+							setCart(addBooksToCart(cart, selectedBook));
+							setSelectedBook([]);
+							successNotification('カートに追加しました');
+							navigate('/home/cart');
+						}}
+					>
+						カートに入れる
+					</Button>
+					<Button
+						fz="sm"
+						color="red"
+						onClick={() => {
+							open();
+						}}
+					>
+						削除する
+					</Button>
+					<Button
+						fz="sm"
+						variant="light"
+						bd="solid 2px"
+						onClick={() => setSelectedBook([])}
+					>
+						選択を解除する
+					</Button>
+				</Stack>
+			</Dialog>
+		</>
 	);
 };
 
