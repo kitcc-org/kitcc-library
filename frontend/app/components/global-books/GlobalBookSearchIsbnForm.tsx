60fb5c24ef7a304b77b4805468fde7b0995509a5
shunsei <k.shunsei1227@gmail.com>
2024-12-14 13:17:27 +0900

    Add barcode scan button to global search page (#46)

--- frontend/app/components/global-books/GlobalBookSearchIsbnForm.tsx
+++ frontend/app/components/global-books/GlobalBookSearchIsbnForm.tsx
@@ -1,6 +1,9 @@
-import { TextInput } from '@mantine/core';
+import { Button, Group, Text, TextInput, rem } from '@mantine/core';
 import type { UseFormReturnType } from '@mantine/form';
+import { useDisclosure } from '@mantine/hooks';
 import type { SearchGoogleBooksParams } from 'client/client.schemas';
+import { MdCameraAlt } from 'react-icons/md';
+import IsbnScanModal from '../common/isbn-scan-modal/IsbnScanModal';
 
 interface GlobalSearchIsbnFormProps {
 	form: UseFormReturnType<
@@ -10,13 +13,28 @@ interface GlobalSearchIsbnFormProps {
 }
 
 const GlobalBookSearchIsbnForm = ({ form }: GlobalSearchIsbnFormProps) => {
+	const [opened, { open, close }] = useDisclosure();
+
 	return (
-		<TextInput
-			label="ISBN"
-			placeholder="10桁または13桁のISBN"
-			key={form.key('isbn')}
-			{...form.getInputProps('isbn')}
-		/>
+		<>
+			<IsbnScanModal url="/home/global" disclosure={{ opened, close }} />
+			<TextInput
+				label={
+					<Group>
+						<Text>ISBN</Text>
+						<Button variant="transparent" onClick={open}>
+							<MdCameraAlt size={24} />
+							<Text td="underline" ml={rem(5)}>
+								バーコードを読み取る
+							</Text>
+						</Button>
+					</Group>
+				}
+				placeholder="10桁または13桁のISBN"
+				key={form.key('isbn')}
+				{...form.getInputProps('isbn')}
+			/>
+		</>
 	);
 };
 
